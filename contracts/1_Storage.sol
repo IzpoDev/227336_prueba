d42fc665698f399a6b7f90eadcb8d02bd2a60789
IzpoDev <ponce.1714.frp@gmail.com>
2026-04-18 20:57:28 -0500

    227336 carga inicial

---
+++ contracts/1_Storage.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract Storage {
+
+    uint256 number; //Este es un comentario de una sola linea
+    /*
+        Este es un comentario de multiples lineas
+    */
+   
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    
+    function retrieve() public view returns (uint256){
+        return number;
+    }
+}
